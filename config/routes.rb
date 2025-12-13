6f576a25680459e069432f823a99e3e995e0fc37
Paulo Sérgio <paulo.leite@academico.ifpb.edu.br>
2025-12-13 14:13:29 -0300

    feature: separated controller into namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :menu_items
-  resources :menus
+  namespace :api do
+    namespace :v1 do
+      resources :menus
+      resources :menu_items
+    end
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
